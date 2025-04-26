0df3c48429fc25766f85d103ee6a545b2cf5c260
sleepy1009 <blockmine223@gmail.com>
2025-04-26 15:43:20 +0700

    idk

--- server/src/server.js
+++ server/src/server.js
@@ -9,8 +9,12 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['https://tracnghiem-wrj7.onrender.com', 'http://localhost:3000'],
-  credentials: true
+  origin: process.env.NODE_ENV === 'production'
+    ? ['https://tracnghiem-wrj7.onrender.com']
+    : ['http://localhost:3000'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 
